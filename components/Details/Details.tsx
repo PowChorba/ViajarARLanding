43c964c4fb7af9bd4a9264455445aa1ef04663bf
PowChorba <101152834+PowChorba@users.noreply.github.com>
2024-07-24 00:45:14 -0300

    changed icons in text

--- components/Details/Details.tsx
+++ components/Details/Details.tsx
@@ -2,6 +2,9 @@ import Image from 'next/image'
 import s from './Details.module.css'
 import DetailOne from '../../assets/detalle_1.webp'
 import Bell from '../../assets/campana.webp'
+import { IoSearchOutline, IoChatboxEllipsesOutline  } from "react-icons/io5";
+import { PiCarLight } from "react-icons/pi";
+import { CiUser } from "react-icons/ci";
 
 export default function Details(){
     return(
@@ -11,14 +14,18 @@ export default function Details(){
                 <div className={s.textContainer}>
                     <div>
                         <div className={s.titleBell}>
-                            <Image src={Bell} alt='Bell' width={48} height={48}/>
-                            <h4>Buscá  viajes más facilmente.</h4>
+                            <div className={s.divIcon}>
+                                <IoSearchOutline size={24} color='white'/>
+                            </div>
+                            <h4>Buscá viajes más facilmente.</h4>
                         </div>
-                        <p>¡No más busquedas interminables en grupos de Facebook y Whatsapp! Con ViajAR, podés buscar viajes basados en tus preferencias de manera más practica.</p>
+                        <p>¿Necesitas ir de una ciudad a otra? Nuestra aplicación te conecta con conductores y pasajeros que comparten rutas similares para el mismo lado. Encontra un viaje en cuestión de minutos.</p>
                     </div>
                     <div>
                         <div className={s.titleBell}>
-                            <Image src={Bell} alt='Bell' width={48} height={48}/>
+                            <div className={s.divIcon}>
+                                <PiCarLight size={24} color='white'/>
+                            </div>
                             <h4>Conectá con viajeros de manera más rapida.</h4>
                         </div>
                         <p>Publicá tus intenciones de viajes para que otros usuarios las puedan encontrar.</p>
@@ -30,14 +37,18 @@ export default function Details(){
                     <div>
                         <div className={s.titleBell}>
                             <h4>Aprovechá el chat integrado en la aplicación.</h4>
-                            <Image src={Bell} alt='Bell' width={48} height={48}/>
+                            <div className={s.divIcon}>
+                                <IoChatboxEllipsesOutline size={24} color='white'/>
+                            </div>
                         </div>
                         <p>Programá los viajes de manera eficiente. Los pasajeros y los conductores pueden colaborar juntos en un solo chat para una planificación conveniente.</p>
                     </div>
                     <div>
                         <div className={s.titleBell}>
                             <h4>Viajá con confianza y tranquilidad.</h4>
-                            <Image src={Bell} alt='Bell' width={48} height={48}/>
+                            <div className={s.divIcon}>
+                                <CiUser size={24} color='white'/>
+                            </div>
                         </div>
                         <p>En ViajAR, valoramos conocer a nuestros usuarios. Revisamos cuidadosamente las opiniones y perfiles de cada uno para que sepas con quién vas a compartir tu viaje. Podes reservar tu próximo trayecto con total seguridad y tranquilidad.</p>
                     </div>
@@ -48,14 +59,18 @@ export default function Details(){
                 <div className={s.textContainer}> 
                     <div>
                         <div className={s.titleBell}>
-                            <Image src={Bell} alt='Bell' width={48} height={48}/>
+                            <div className={s.divIcon}>
+                                <IoChatboxEllipsesOutline size={24} color='white'/>
+                            </div>
                             <h4>Aprovechá el chat integrado en la aplicación.</h4>
                         </div>
                         <p>Programá los viajes de manera eficiente. Los pasajeros y los conductores pueden colaborar juntos en un solo chat para una planificación conveniente.</p>
                     </div>
                     <div>
                         <div className={s.titleBell}>
-                            <Image src={Bell} alt='Bell' width={48} height={48}/>
+                            <div className={s.divIcon}>
+                                <CiUser size={24} color='white'/>
+                            </div>
                             <h4>Viajá con confianza y tranquilidad.</h4>
                         </div>
                         <p>En ViajAR, valoramos conocer a nuestros usuarios. Revisamos cuidadosamente las opiniones y perfiles de cada uno para que sepas con quién vas a compartir tu viaje. Podes reservar tu próximo trayecto con total seguridad y tranquilidad.</p>
